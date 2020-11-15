a46c22732b7601974a08b7301fbe0c79c460af07
rostamiani <mahdi.temps@gmail.com>
2020-11-15 08:24:36 +0330

    Init cassandra

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+
+// import cassandra from 'cassandra-driver';
+import { Client } from 'cassandra-driver';
+
+console.log("Initializing Benchmark");
+
+const db = new Client({
+    // contactPoints: ['h1', 'h2'],
+    // localDataCenter: 'datacenter1',
+    keyspace: 'skyroom`'
+})
+
+// db.execute("select * from users",result=>{
+//     console.log(result);
+// })
